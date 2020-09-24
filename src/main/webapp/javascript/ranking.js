4821c97580167e7122995e0c1ef27d0454d5d1b1
Admin <Admin@DESKTOP-G2QUO26>
2020-09-24 17:23:46 +0300

    Ranking updated

--- src/main/webapp/javascript/ranking.js
+++ src/main/webapp/javascript/ranking.js
@@ -1,20 +1,54 @@
 $(function() {
-	
+
 	getAllUsers = function() {
 		$.ajax({
 			method : "GET",
 			url : "getAllUsersExceptAdmin"
-		}).done(
-				function(response) {
-					console.log(response);
-					sortRanking(response);
+		}).done(function(response) {
+			sortRanking(response);
 
-				}).fail(function(response) {
+		}).fail(function(response) {
 		})
 	}
-	
-	sortRanking = function(response){
-		response.sort(function(a,b){
+
+	getUserInfo = function(rankings) {
+		$.ajax({
+			method : "GET",
+			url : "getCurrentUser"
+		}).done(
+				function(response) {
+					if (!response) {
+						window.location = "/";
+						return;
+					}
+					var $ingameName = $(".user-ingame-name");
+					$ingameName.text(response.ingameName);
+					var $guildPoints = $(".user-guildpoints");
+					$guildPoints.text(response.guildPoints);
+					for (var i = 0; i < rankings.length; i++) {
+						var currentUser = rankings[i];
+						if (currentUser.id == response.id) {
+							var currentRanking = i + 1;
+							$(".user-rank").text(
+									"Your current rank on the ladder is "
+											+ currentRanking + " !");
+							if (currentRanking == 1) {
+								$("#user-badge").attr("src",
+										'../../images/Rank_One.png');
+							} else if (currentRanking == 2) {
+								$("#user-badge").attr("src",
+										'../../images/Rank_Two.png');
+							} else if (currentRanking == 3) {
+								$("#user-badge").attr("src",
+										'../../images/Rank_Three.png');
+							}
+						}
+					}
+				});
+	}
+
+	sortRanking = function(response) {
+		response.sort(function(a, b) {
 			return b.guildPoints - a.guildPoints;
 		});
 		for (var i = 0; i < response.length; i++) {
@@ -22,9 +56,10 @@ $(function() {
 			renderRanking(currentUser.ingameName, currentUser.guildPoints);
 
 		}
-		console.log(response);
+		var rankings = response;
+		getUserInfo(rankings);
 	}
-	
+
 	var renderRanking = function(ingameName, guildPoints) {
 
 		var $template = $('#template-ranking').html();
